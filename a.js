6bc1f150db2cba94285be71fdc630755f68373e1
rakibulhasan55 <rakibulhasan2355@gmail.com>
2026-05-16 22:38:20 +0600

    JS Exam

--- a.js
+++ a.js
@@ -1,8 +1,14 @@
-const age = 27;
+function validateIdentity(CIN) {
 
-if (age >= 18) {
-  console.log("Adult");
-} else {
-  console.log("Not Adult");
+ 
+  for (let i = 2; i < CIN; i++) { // loop from 2 to CIN-1 to check for any divisors
+
+    if (CIN % i === 0) { // If CIN is divisible by i (remainder is 0), then CIN is NOT a prime number
+      console.log("Agent Approved: Identity pattern is untraceable.");
+      return; 
+    }
+  }
+  console.log("Rejected: Identity pattern shows external links.");
 }
+console.log(validateIdentity(91));
   
